687068397d84841b1e497475b3c88ddcf8281197
Sid Kandasamy <sidkandasamy@outlook.com>
2020-02-18 14:17:10 +0000

    initial set up complete

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>hello world</h1>
+       <h1>Cash Converter</h1>
       </header>
     </div>
   );
