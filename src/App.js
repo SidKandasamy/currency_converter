43b95f1778bed0a69f57ab04e8759a8f14cda5cd
Sid Kandasamy <sidkandasamy@outlook.com>
2020-02-18 14:10:45 +0000

    problem resolved in app.js for deployment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello world</h1>
+       <h1>hello world</h1>
       </header>
     </div>
   );
