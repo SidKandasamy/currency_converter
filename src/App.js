e1eb75a170c2389447bf949738b3d8ab75f15916
Sid Kandasamy <sidkandasamy@outlook.com>
2020-02-18 14:21:36 +0000

    new component works

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import './CurrencyRow';
+import CurrencyRow from './CurrencyRow';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <h1>Cash Converter</h1>
+       <CurrencyRow/>
       </header>
     </div>
   );
---
+++ src/CurrencyRow.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function CurrencyRow() {
+    return (
+        <div>
+            <h1>Currency Row</h1>
+        </div>
+    )
+}
+export default CurrencyRow
