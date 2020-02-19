8ec4909653a9d0b1127c87a28f2e686359888d70
Sid Kandasamy <sidkandasamy@outlook.com>
2020-02-19 13:51:28 +0000

    const for latest exchange rates found

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import './CurrencyRow';
 import CurrencyRow from './CurrencyRow';
 
+//https://exchangeratesapi.io/
+const BASE_URL = 'https://api.exchangeratesapi.io/latest'
+
 function App() {
+
+  useEffect(()=> {
+
+  }, [])
+  //useEffect is a function used from the react Library, pass through two arguments one empty array and the other a function
+
   return (
       <div>
         <h1>Cash Converter</h1>
