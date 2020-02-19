6f7f71ef4c2ee651f4c6b0480a36c3e4a92a830c
Sid Kandasamy <sidkandasamy@outlook.com>
2020-02-19 14:11:07 +0000

    select option working with props

--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
 function CurrencyRow(props) {
+
+    const {
+        currencyOptions
+    } = props
+
     return (
         <div>
             <input className="input" type="number"/> {/* number type in input meaning only numbers can be input */} 
             <select>
-                <option value="Hi">Hi</option>
+                {currencyOptions.map(options=> (
+                    <option value={options}>{options}</option>
+                ))}
             </select>
         </div>
     )
