53604318c763ed02fcf6eaf9b516481732b6a2f8
Sid Kandasamy <sidkandasamy@outlook.com>
2020-02-19 13:55:46 +0000

    json is logged into the console - all currency data stored

--- src/App.js
+++ src/App.js
@@ -9,10 +9,12 @@ const BASE_URL = 'https://api.exchangeratesapi.io/latest'
 function App() {
 
   useEffect(()=> {
-
+      fetch(BASE_URL)
+      .then(res=>res.json())
+      .then(data => console.log(data))
   }, [])
   //useEffect is a function used from the react Library, pass through two arguments one empty array and the other a function
-
+  // fetch base URL, then convert response to json
   return (
       <div>
         <h1>Cash Converter</h1>
